5137ba96087ba297e19f4d59157afdf36bc25889
67tasman <league@WTS10.attlocal.net>
2018-03-10 15:57:21 -0800

    League Invaders

--- src/intro_to_array_lists/LeagueInvaders.java
+++ src/intro_to_array_lists/LeagueInvaders.java
@@ -1,10 +1,34 @@
 package intro_to_array_lists;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class LeagueInvaders {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	JFrame frame;
+GamePanel panel;
+	LeagueInvaders() {
+		frame = new JFrame();
+		panel = new GamePanel();
 	}
 
+	final int w = 500;
+	final int h = 800;
+
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
+		// TODO Auto-generated method stub
+	}
+
+	void setup() {
+		frame.add(panel);
+		frame.setSize(w, h);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setPreferredSize(new Dimension(w, h));
+        frame.pack();
+        panel.StartGame();
+        frame.addKeyListener(panel);
+	}
 }
